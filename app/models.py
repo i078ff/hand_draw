1ce2fbb46ab1a5cdd83acfadf04c45ef6c7266e6
yochimonji <yochimonji@gmail.com>
2021-07-07 23:56:29 +0900

    add #6 docker-compose.ymlの環境変数にPOSTGRES_DBを追加

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from app.app import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:passward@postgres-server:5432/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@postgres-server:5432/test'
 db = SQLAlchemy(app)
