99a71c83e43a646889db4319ae65dfb7e7df8e18
yochimonji <54769623+yochimonji@users.noreply.github.com>
2021-07-08 11:03:13 +0900

    add #6 環境構築を完了した (#7)
    * add #6 docker-compose.ymlを追加した
    
    * add #6 run.py追加
    
    * add #6 SQLへ接続するpyファイル追加
    
    * add #6 DB初期化を追加
    
    * add #6 postgreのDockerfile追加
    
    * update #6 Pipfileをrequirements.txtに変更
    
    * update #6 appのDockerfileでrequirements.txtを使用するよう更新
    
    * add #6 app.pyを作成
    
    * add #6 nginxのDockerfile追加
    
    * add #6 nginx.conf追加
    
    * update #6  微調整
    
    * add #6 docker-compose.ymlの環境変数にPOSTGRES_DBを追加
    
    * update #6 requirements.txtのパスを変更
    
    * fix #6 ライブラリインストールをコンテナ起動時からビルド時に変更
    
    * update #6 restartを追加してコンテナ起動時に接続できない問題を解決
    
    * update #6 black+flake8を導入
    
    * update black+flake8を導入

---
+++ app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+from app.models import db
+
+db.create_all()
+db.session.commit()
+
+
+@app.route("/")
+def index():
+    return "success"
---
+++ postgres/initdb/createdb.sql
@@ -0,0 +1,3 @@
+create table test (
+    user_id integer
+);
