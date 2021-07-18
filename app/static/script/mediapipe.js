fed1ca208b442da560d222bd1d2e3ed1a564f4e3
yochimonji <54769623+yochimonji@users.noreply.github.com>
2021-07-18 21:45:52 +0900

    Mediapipe (#25)
    * remove テストファイルの削除
    
    * add mediapipeのテストを追加
    
    * remove mediapipeのテストファイルを削除
    
    * update index2にmediapipeを追加
    
    mainにmediapipeを追加&隠しメニューをnoneに設定
    
    * remove stylesheet.cssとindexx2.htmlを削除
    
    * update 手の領域を取得して手のみ描画
    
    * update 左右反転対応
    
    実際に手を動かした方向と画面上で動かした方向が逆だったため左右反転した
    
    * update 手の抽出領域拡大
    
    手が見切れるので抽出領域を10%ずつ拡大した
    
    * update windowの大きさの変更に対応
    
    途中でwindowの大きさを変更しても正しく表示できるように変更
    
    * update output_canvasをoperation_canvasに変更
    
    * update 画面中央に描画用canvasを作成
    
    * fix 描画エリアの縦の位置の修正
    
    * update 線を描画できるようにした
    
    関数化もした。windowサイズを変更した時に線が反転するバグあり
    
    * update canvasから手が見切れた時の処理
    
    手が見切れて戻ってきた時に直線が惹かれてしまう問題を修正
    
    * update 関数化＆変数名変更
    
    一部処理を関数化した、より相応しい変数名に変更した

---
+++ app/static/script/mediapipe.js
@@ -0,0 +1,106 @@
+window.addEventListener(
+    'DOMContentLoaded', () => {
+        const videoElement = document.getElementsByClassName('input_video')[0];
+        const operationCanvasElement = document.getElementsByClassName('operation_canvas')[0];
+        const operationCanvasCtx = operationCanvasElement.getContext('2d');
+        const drawCanvasElement = document.getElementById('draw_canvas');
+        const drawCanvasCtx = drawCanvasElement.getContext('2d');
+        let isInDrawCanvas = false;
+        drawCanvasElement.width = 900;
+        drawCanvasElement.height = 600;
+        init();
+
+        function init() {
+            operationCanvasElement.width = window.innerWidth;
+            operationCanvasElement.height = window.innerHeight;
+            operationCanvasCtx.scale(-1, 1);
+            operationCanvasCtx.translate(-operationCanvasElement.width, 0);
+            drawCanvasCtx.scale(-1, 1);
+            drawCanvasCtx.translate(-drawCanvasElement.width, 0);
+        }
+
+        function onResults(results) {
+            operationCanvasCtx.clearRect(0, 0, operationCanvasElement.width, operationCanvasElement.height);
+            if (results.multiHandLandmarks) {
+                for (const landmarks of results.multiHandLandmarks) {
+                    const indexCoordinate = { 'x': landmarks[8]['x'] * operationCanvasElement.width, 'y': landmarks[8]['y'] * operationCanvasElement.height };
+                    drawLine(indexCoordinate);
+                    drawHandRegion(results.image, landmarks);
+                    // 各ランドマークを表示
+                    // drawConnectors(operationCanvasCtx, landmarks, HAND_CONNECTIONS,
+                    //     { color: '#00FF00', lineWidth: 5 });
+                    // drawLandmarks(operationCanvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
+                }
+            }
+        }
+
+        function drawLine(indexCoordinate) {
+            const drawRect = drawCanvasElement.getBoundingClientRect();
+            if ((drawRect.left < indexCoordinate.x) && (indexCoordinate.x < drawRect.right) && (drawRect.top < indexCoordinate.y) && (indexCoordinate.y < drawRect.bottom)) {
+                if (!isInDrawCanvas) {
+                    isInDrawCanvas = true;
+                    drawCanvasCtx.beginPath();
+                }
+                drawCanvasCtx.lineWidth = 5;
+                drawCanvasCtx.lineTo(indexCoordinate.x - drawRect.left, indexCoordinate.y - drawRect.top);
+                drawCanvasCtx.stroke();
+            } else {
+                if (isInDrawCanvas) {
+                    isInDrawCanvas = false;
+                    drawCanvasCtx.closePath();
+                }
+            }
+        }
+
+        function drawHandRegion(image, landmarks) {
+            const [dx, dy, dw, dh] = getHandRegion(landmarks);
+            operationCanvasCtx.drawImage(
+                image, image.width * dx, image.height * dy, image.width * dw, image.height * dh,
+                operationCanvasElement.width * dx, operationCanvasElement.height * dy, operationCanvasElement.width * dw, operationCanvasElement.height * dh);
+        }
+
+        function getHandRegion(landmarks) {
+            const xList = [];
+            const yList = [];
+            for (let i = 0; i < landmarks.length; i++) {
+                xList.push(landmarks[i]['x']);
+                yList.push(landmarks[i]['y']);
+            }
+            let x = Math.min(...xList);
+            let y = Math.min(...yList);
+            let w = Math.max(...xList) - x;
+            let h = Math.max(...yList) - y;
+
+            // このままのx, y, w, hだと手が見切れるので上下左右10%ずつ領域を広げる
+            x = x - w / 10;
+            y = y - h / 10;
+            w = w * 1.2;
+            h = h * 1.2;
+            return [x, y, w, h];
+        }
+
+        const hands = new Hands({
+            locateFile: (file) => {
+                return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+            }
+        });
+        hands.setOptions({
+            maxNumHands: 1,
+            minDetectionConfidence: 0.5,
+            minTrackingConfidence: 0.5
+        });
+        hands.onResults(onResults);
+
+        let camera = new Camera(videoElement, {
+            onFrame: async () => {
+                await hands.send({ image: videoElement });
+            },
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+        camera.start();
+
+        window.addEventListener('resize', () => {
+            init();
+        }, false);
+    }, false);
