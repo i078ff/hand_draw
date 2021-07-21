c9c16930d431930ed7b24fb55cabb848989786fa
yochimonji <54769623+yochimonji@users.noreply.github.com>
2021-07-21 11:27:44 +0900

    Operate dom (#30)
    * add firebaseConfig.json追加
    
    * update .gitignore更新
    
    pictures追加とfirebaseConfig.jsonをfirebaseConfig.jsに修正
    
    * fix リファクタリング
    
    関数の順番を変更＆コメントの削除＆drawページの手を描画しないように設定した
    
    * update DOM操作を非同期にした＆人差し指の位置にポインタを表示した
    
    * update 誤って線を描画する可能性を軽減
    
    識別結果をキューに入れることで連続でハンドサインを認識しないと線を描画できないようにした
    
    * update indexかそれ以外かでポインターの色を変更
    
    * update 色ボタンにポインターオーバーで色メニュー表示
    
    * update 隠しメニュー表示
    
    * update 色の変更ができるようになった
    
    * update htmlとcssを調整
    
    * update 太さに変更
    
    * update clearボタンの処理を実装
    
    * update logout, help, homeボタンを繋げた
    
    * update helpを正しいものに更新

--- app/static/script/mediapipe.js
+++ app/static/script/mediapipe.js
@@ -1,7 +1,17 @@
+import { firebaseConfig } from './firebaseConfig.js';
 import { initModel, runModel } from './onnx.js';
 
 window.addEventListener(
     'DOMContentLoaded', () => {
+        const menuColor = document.getElementsByClassName('menu-color')[0];
+        const colorList = document.getElementsByClassName('color');
+        const menuThickness = document.getElementsByClassName('menu-thickness')[0];
+        const thicknessList = document.getElementsByClassName('thickness');
+        const menuClear = document.getElementsByClassName('menu-clear')[0];
+        const menuSetting = document.getElementsByClassName('menu-setting')[0];
+        const buttonLogout = document.getElementsByClassName('logout')[0];
+        const buttonHelp = document.getElementsByClassName('help')[0];
+        const buttonHome = document.getElementsByClassName('home')[0];
         const videoElement = document.getElementsByClassName('input_video')[0];
         const operationCanvasElement = document.getElementsByClassName('operation_canvas')[0];
         const operationCanvasCtx = operationCanvasElement.getContext('2d');
@@ -13,14 +23,16 @@ window.addEventListener(
         drawCanvasElement.height = 600;
         handCanvasElement.width = 224;
         handCanvasElement.height = 224;
-        let isInDrawCanvas = false;
+        let colorFlag = false;
+        let thicknessFlag = false;
+        let settingFlag = false;
+        let lineFlag = false;
         const modelFile = './static/squeezenet1_1_224_886.onnx';
         const labels = ['all', 'index', 'index_middle', 'index_thumb', 'other'];
+        const labelQueue = ['other', 'other', 'other', 'other', 'other'];
         let session;
         init();
-
-        // モデルの初期化
-        initModel(modelFile).then(output => {
+        initModel(modelFile).then(output => {  // モデルの初期化
             session = output;
         })
 
@@ -31,6 +43,7 @@ window.addEventListener(
             operationCanvasCtx.translate(-operationCanvasElement.width, 0);
             drawCanvasCtx.scale(-1, 1);
             drawCanvasCtx.translate(-drawCanvasElement.width, 0);
+            drawCanvasCtx.lineWidth = 7;
             handCanvasCtx.scale(-1, 1);
             handCanvasCtx.translate(-handCanvasElement.width, 0);
         }
@@ -40,43 +53,31 @@ window.addEventListener(
             handCanvasCtx.clearRect(0, 0, handCanvasElement.width, handCanvasElement.height);
             if (results.multiHandLandmarks) {
                 for (const landmarks of results.multiHandLandmarks) {
-                    const indexCoordinate = { 'x': landmarks[8]['x'] * operationCanvasElement.width, 'y': landmarks[8]['y'] * operationCanvasElement.height };
-                    drawLine(indexCoordinate);
-                    drawHandRegion(results.image, landmarks);
-                    runModel(session, handCanvasCtx).then(output => {
-                        // console.log(labels[output.data.indexOf(Math.max(...output.data))]);
-                    });
-                    // 各ランドマークを表示
-                    // drawConnectors(operationCanvasCtx, landmarks, HAND_CONNECTIONS,
-                    //     { color: '#00FF00', lineWidth: 5 });
-                    // drawLandmarks(operationCanvasCtx, landmarks, { color: '#FF0000', lineWidth: 2 });
+                    operateDOM(results, landmarks);
                 }
             }
         }
 
-        function drawLine(indexCoordinate) {
-            const drawRect = drawCanvasElement.getBoundingClientRect();
-            if ((drawRect.left < indexCoordinate.x) && (indexCoordinate.x < drawRect.right) && (drawRect.top < indexCoordinate.y) && (indexCoordinate.y < drawRect.bottom)) {
-                if (!isInDrawCanvas) {
-                    isInDrawCanvas = true;
-                    drawCanvasCtx.beginPath();
-                }
-                drawCanvasCtx.lineWidth = 5;
-                drawCanvasCtx.lineTo(indexCoordinate.x - drawRect.left, indexCoordinate.y - drawRect.top);
-                drawCanvasCtx.stroke();
-            } else {
-                if (isInDrawCanvas) {
-                    isInDrawCanvas = false;
-                    drawCanvasCtx.closePath();
-                }
-            }
+        async function operateDOM(results, landmarks) {
+            const indexCoordinate = { 'x': landmarks[8]['x'] * operationCanvasElement.width, 'y': landmarks[8]['y'] * operationCanvasElement.height };
+            await drawHandRegion(results.image, landmarks);
+            const output = await runModel(session, handCanvasCtx)
+            const label = labels[output.data.indexOf(Math.max(...output.data))];
+            labelQueue.shift();
+            labelQueue.push(label);
+            console.log(labelQueue);
+            await Promise.all([
+                drawPointer(indexCoordinate),
+                chooseColor(indexCoordinate),
+                chooseThickness(indexCoordinate),
+                clickClear(indexCoordinate),
+                chooseSetting(indexCoordinate)
+            ]);
+            await drawLine(indexCoordinate)
         }
 
         function drawHandRegion(image, landmarks) {
             const [dx, dy, dw, dh] = getHandRegion(landmarks);
-            operationCanvasCtx.drawImage(
-                image, image.width * dx, image.height * dy, image.width * dw, image.height * dh,
-                operationCanvasElement.width * dx, operationCanvasElement.height * dy, operationCanvasElement.width * dw, operationCanvasElement.height * dh);
             handCanvasCtx.drawImage(
                 image, image.width * dx, image.height * dy, image.width * dw, image.height * dh,
                 0, 0, handCanvasElement.width, handCanvasElement.height);
@@ -100,6 +101,173 @@ window.addEventListener(
             w = w * 1.2;
             h = h * 1.2;
             return [x, y, w, h];
+        }
+
+        function drawPointer(indexCoordinate) {
+            operationCanvasCtx.beginPath();
+            operationCanvasCtx.arc(indexCoordinate.x, indexCoordinate.y, 10, 0, 2 * Math.PI, false);
+            if (labelQueue.length === labelQueue.filter(label => label === 'index').length) {
+                operationCanvasCtx.fillStyle = 'rgba(50, 50, 50, 127)';
+            } else {
+                operationCanvasCtx.fillStyle = 'rgba(127, 127, 127, 127)';
+            }
+            operationCanvasCtx.fill();
+            operationCanvasCtx.stroke();
+        }
+
+        function chooseColor(indexCoordinate) {
+            const menuColorRect = menuColor.getBoundingClientRect();
+            if ((menuColorRect.left < operationCanvasElement.width - indexCoordinate.x) &&
+                (operationCanvasElement.width - indexCoordinate.x < menuColorRect.right) &&
+                (menuColorRect.top < indexCoordinate.y) &&
+                (indexCoordinate.y < menuColorRect.bottom)) {
+                if (!colorFlag) {
+                    colorFlag = true;
+                    document.getElementsByClassName('button-color-hide')[0].style.display = 'flex';
+                }
+                for (let i = 0; i < colorList.length; i++) {
+                    const colorRect = colorList[i].getBoundingClientRect();
+                    if ((colorRect.left < operationCanvasElement.width - indexCoordinate.x) &&
+                        (operationCanvasElement.width - indexCoordinate.x < colorRect.right) &&
+                        (colorRect.top < indexCoordinate.y) &&
+                        (indexCoordinate.y < colorRect.bottom)) {
+                        drawCanvasCtx.strokeStyle = window.getComputedStyle(colorList[i]).getPropertyValue('background-color');
+                    }
+                }
+            } else {
+                if (colorFlag) {
+                    colorFlag = false;
+                    document.getElementsByClassName('button-color-hide')[0].style.display = 'none';
+                }
+            }
+        }
+
+        function chooseThickness(indexCoordinate) {
+            const menuThicknessRect = menuThickness.getBoundingClientRect();
+            if ((menuThicknessRect.left < operationCanvasElement.width - indexCoordinate.x) &&
+                (operationCanvasElement.width - indexCoordinate.x < menuThicknessRect.right) &&
+                (menuThicknessRect.top < indexCoordinate.y) &&
+                (indexCoordinate.y < menuThicknessRect.bottom)) {
+                if (!thicknessFlag) {
+                    thicknessFlag = true;
+                    document.getElementsByClassName('button-thickness-hide')[0].style.display = 'flex';
+                }
+                for (let i = 0; i < thicknessList.length; i++) {
+                    const colorRect = thicknessList[i].getBoundingClientRect();
+                    if ((colorRect.left < operationCanvasElement.width - indexCoordinate.x) &&
+                        (operationCanvasElement.width - indexCoordinate.x < colorRect.right) &&
+                        (colorRect.top < indexCoordinate.y) &&
+                        (indexCoordinate.y < colorRect.bottom)) {
+                        if (i === 0) {
+                            drawCanvasCtx.lineWidth = 2;
+                        } else if (i === 1) {
+                            drawCanvasCtx.lineWidth = 7;
+                        } else {
+                            drawCanvasCtx.lineWidth = 15;
+                        }
+                    }
+                }
+            } else {
+                if (thicknessFlag) {
+                    thicknessFlag = false;
+                    document.getElementsByClassName('button-thickness-hide')[0].style.display = 'none';
+                }
+            }
+        }
+
+        function clickClear(indexCoordinate) {
+            const menuClearRect = menuClear.getBoundingClientRect();
+            if ((labelQueue.length === labelQueue.filter(label => label === 'index').length) &&
+                (menuClearRect.left < operationCanvasElement.width - indexCoordinate.x) &&
+                (operationCanvasElement.width - indexCoordinate.x < menuClearRect.right) &&
+                (menuClearRect.top < indexCoordinate.y) &&
+                (indexCoordinate.y < menuClearRect.bottom)) {
+                drawCanvasCtx.clearRect(0, 0, drawCanvasElement.width, drawCanvasElement.height);
+            }
+        }
+
+        function chooseSetting(indexCoordinate) {
+            const menuSettingRect = menuSetting.getBoundingClientRect();
+            if ((menuSettingRect.left < operationCanvasElement.width - indexCoordinate.x) &&
+                (operationCanvasElement.width - indexCoordinate.x < menuSettingRect.right) &&
+                (menuSettingRect.top < indexCoordinate.y) &&
+                (indexCoordinate.y < menuSettingRect.bottom)) {
+                if (!settingFlag) {
+                    settingFlag = true;
+                    document.getElementsByClassName('button-setting-hide')[0].style.display = 'flex';
+                }
+                clickLogout(indexCoordinate);
+                clickHelp(indexCoordinate);
+                clickHome(indexCoordinate);
+            } else {
+                if (settingFlag) {
+                    settingFlag = false;
+                    document.getElementsByClassName('button-setting-hide')[0].style.display = 'none';
+                }
+            }
+        }
+
+        function clickLogout(indexCoordinate) {
+            const buttonLogoutRect = buttonLogout.getBoundingClientRect();
+            if ((labelQueue.length === labelQueue.filter(label => label === 'index').length) &&
+                (buttonLogoutRect.left < operationCanvasElement.width - indexCoordinate.x) &&
+                (operationCanvasElement.width - indexCoordinate.x < buttonLogoutRect.right) &&
+                (buttonLogoutRect.top < indexCoordinate.y) &&
+                (indexCoordinate.y < buttonLogoutRect.bottom)) {
+                firebase.auth().signOut().then(() => {
+                    // Sign-out successful.
+                    window.location.href = '/login';
+                }).catch((error) => {
+                    // An error happened.
+                });
+            }
+        }
+
+        function clickHelp(indexCoordinate) {
+            const buttonHelpRect = buttonHelp.getBoundingClientRect();
+            if ((labelQueue.length === labelQueue.filter(label => label === 'index').length) &&
+                (buttonHelpRect.left < operationCanvasElement.width - indexCoordinate.x) &&
+                (operationCanvasElement.width - indexCoordinate.x < buttonHelpRect.right) &&
+                (buttonHelpRect.top < indexCoordinate.y) &&
+                (indexCoordinate.y < buttonHelpRect.bottom)) {
+                window.location.href = '/help';
+            }
+        }
+
+        function clickHome(indexCoordinate) {
+            const buttonHomeRect = buttonHome.getBoundingClientRect();
+            if ((labelQueue.length === labelQueue.filter(label => label === 'index').length) &&
+                (buttonHomeRect.left < operationCanvasElement.width - indexCoordinate.x) &&
+                (operationCanvasElement.width - indexCoordinate.x < buttonHomeRect.right) &&
+                (buttonHomeRect.top < indexCoordinate.y) &&
+                (indexCoordinate.y < buttonHomeRect.bottom)) {
+                window.location.href = '/temp';
+            }
+        }
+
+        function drawLine(indexCoordinate) {
+            const drawRect = drawCanvasElement.getBoundingClientRect();
+            // 5連続index判定かつ隠しメニュー非表示かつindexの座標がdrawCanvas内の場合実行
+            if ((labelQueue.length === labelQueue.filter(label => label === 'index').length) &&
+                (!colorFlag) &&
+                (!thicknessFlag) &&
+                (!settingFlag) &&
+                (drawRect.left < indexCoordinate.x) &&
+                (indexCoordinate.x < drawRect.right) &&
+                (drawRect.top < indexCoordinate.y) &&
+                (indexCoordinate.y < drawRect.bottom)) {
+                if (!lineFlag) {
+                    lineFlag = true;
+                    drawCanvasCtx.beginPath();
+                }
+                drawCanvasCtx.lineTo(indexCoordinate.x - drawRect.left - 12, indexCoordinate.y - drawRect.top);
+                drawCanvasCtx.stroke();
+            } else {
+                if (lineFlag) {
+                    lineFlag = false;
+                    drawCanvasCtx.closePath();
+                }
+            }
         }
 
         const hands = new Hands({
